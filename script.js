54b4135f839bf85770679c563d60c9970bdbb704
TonyLi0916 <tonyli4505@gmail.com>
2025-03-23 16:58:39 -0400

    Added Hover Effect over Squares

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement("div");
-    square.textContent = "box";
     square.style.backgroundColor = "pink";
     square.classList.add("square");
     container.appendChild(square);
 }
+
+const allSquares = document.querySelectorAll(".square");
+
+allSquares.forEach(square => {
+    square.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "purple";
+    });
+});
+
