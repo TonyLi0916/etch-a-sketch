94ff08935e0b965b2f6f624ef2a63955da0352e7
TonyLi0916 <tonyli4505@gmail.com>
2025-03-23 16:39:55 -0400

    Created 16x16 Grid using Flexbox and Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement("div");
+    square.textContent = "box";
+    square.style.backgroundColor = "pink";
+    square.classList.add("square");
+    container.appendChild(square);
+}
