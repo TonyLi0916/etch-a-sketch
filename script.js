7eeb647674395d6d2a8def074b5195e4f9e1e2ba
TonyLi0916 <tonyli4505@gmail.com>
2025-03-23 17:52:59 -0400

    Implemented the newGridSize function which resizes each square depending on input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const newGrid = document.querySelector("#new");
 
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement("div");
@@ -14,4 +15,38 @@ allSquares.forEach(square => {
         e.target.style.backgroundColor = "purple";
     });
 });
+
+newGrid.addEventListener("click", () => {
+    let count = prompt("Please enter new desired Grid Size! (Up to 100)");
+    if (count > 0 && count <= 100) {
+        newGridSize(count);
+    }
+    else {
+        alert("Please re-enter a valid sizing");
+    }
+});
+
+function newGridSize(count) {
+    container.innerHTML = "";
+    
+    const containerSize = 560; 
+    const squareSize = containerSize / count;
+    
+    for (let i = 0; i < count * count; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.backgroundColor = "pink";
+        square.style.width = `${squareSize}px`; 
+        square.style.height = `${squareSize}px`;
+        container.appendChild(square);
+    }
+    
+    const allNewSquares = document.querySelectorAll(".square");
+
+    allNewSquares.forEach(square => {
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "purple";
+        });
+    });
+}
 
